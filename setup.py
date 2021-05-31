b5397bd209e5d57921d3d8bdc9508b387836d5e8
Stephan Peters <stephan.peters@imc.com>
2021-05-31 12:19:16 +0200

    Adding dependencies

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
 from setuptools import setup
 
 setup(name='historicdutchweather',
-      version='0.1',
+      version='0.2',
       description='Download the hourly historic weather from the dutch weather agency KNMI and localize the data to a particular lon/lat',
       url='https://github.com/stephanpcpeters/HourlyHistoricWeather',
       author='Stephan Peters',
       author_email='s.p.c.peters@gmail.com',
       license='MIT',
       packages=['historicdutchweather'],
+      install_requires=[
+          'pandas',
+          'numpy',
+          'tqdm',
+          'scipy'
+      ],
       zip_safe=False)
